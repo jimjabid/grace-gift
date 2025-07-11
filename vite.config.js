56bf623c311004a1066d7c7849beb5248bc0d6e9
jimjabid <jabidandresjimenezserrano@gmail.com>
2025-07-11 03:07:51 -0300

    Update package-lock.json with new dependencies for @tailwindcss/oxide-wasm32-wasi and modify Vite configuration to set base path to '/grace-gift/'

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: "/grace-gift/",
   build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-  }
-})
+    outDir: "dist",
+    assetsDir: "assets",
+  },
+});
